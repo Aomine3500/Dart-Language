17b1040cfde5dfa88098baf98665f07f04fd8640
Ahmed ZNOUDA <42349194+Aomine3500@users.noreply.github.com>
2024-05-29 10:45:50 +0100

    Practice Dart Second Commit

---
+++ lib/Practice Dart/SplitAmount.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Split amount of a restaurant
+
+  int NbrPers = 6;
+  double Total = 1250.500;
+
+  double Par = Total / NbrPers;
+
+  print("The amount part of every persone is : $Par");
+}
---
+++ lib/Practice Dart/TravelTime.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Calculate the travel time
+
+  int distance = 25;
+  int speed = 40;
+
+  double TravTime = (25 / 40) * 60;
+
+  print("The travel time is $TravTime minutes");
+}
